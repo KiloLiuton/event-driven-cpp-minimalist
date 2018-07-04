6752ad9499c9544be0098ba9c41c61505b40e26b
Kevin <kkevin.liu@gmail.com>
2018-07-04 13:39:54 -0300

    add some data analysis notebook

--- build_exe.py
+++ build_exe.py
@@ -8,4 +8,4 @@ p = 0.0
 for i in range(1, 9):
     N = 500*i
     K = int(a*N)
-    subprocess.run(['make', 'N=%d'%N, 'K=%d'%K, 'p=%.1f'%p])
+    subprocess.run(['make', 'N=%d'%N, 'K=%d'%K, 'p=%.3f'%p])
