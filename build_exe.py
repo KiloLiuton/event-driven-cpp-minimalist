2a6d89c5acf854299c9751840c3854b9bc594455
kiloliuton <kkevin.liu@gmail.com>
2018-07-06 09:48:25 -0300

    foo

--- build_exe.py
+++ build_exe.py
@@ -3,13 +3,12 @@ import numpy as np
 import subprocess
 import sys
 
-alpha = float(sys.argv[1])
-n = 5
+alphas = np.linspace(0.01, 0.45, 8)
+n = 4
 x = np.logspace(-4, -1, n)
 
-for i in range(1, n+1):
-    N = 500*i
+for alpha in alphas:
+    N = 1001
     K = int(alpha*N)
     p = 0.0
-    print('p=%f'%p)
     subprocess.run(['make', 'N=%d'%N, 'K=%d'%K, 'p=%6.6f'%p])
