420724fed2a01fe3553bba8af5958bdd50f878df
Liu Rodrigues <kliurodr@biop-kbwood4.biop.lsa.umich.edu>
2018-09-06 15:54:20 -0400

    fix the plotting of a single trial

--- plot_trial.py
+++ plot_trial.py
@@ -7,37 +7,40 @@ import pandas as pd
 from matplotlib.patches import Polygon
 
 for filename in sys.argv[1:]:
-    N = filename[filename.find('N-')+2:filename.find('K-')]
-    K = filename[filename.find('K-')+2:filename.find('p-')]
-    title = 'N=' + N + ' K=' + K
-
-    datap = pd.read_csv(filename, header=1, delimiter=',')
+    datap = pd.read_csv(filename, header=2, delimiter=',')
     print(datap.head())
 
     with open(filename, 'r') as f:
-        header = f.readline()
-    N, K, p, a, BURN, ITERS = header.split(',')
+        graph_params = f.readline()
+        dynamics_params = f.readline()
+    _, N, K, p, seed = graph_params.split(' ')
+    _, a, iters, burn, _, _ = dynamics_params.split(' ')
+    title = N + ' ' + K + ' ' + p + ' ' + a
     N = int(N.split('=')[1])
     K = int(K.split('=')[1])
     p = float(p.split('=')[1])
     a = float(a.split('=')[1])
-    BURN = int(BURN.split('=')[1])
-    ITERS = int(ITERS.split('=')[1])
+    iters = int(iters.split('=')[1])
+    burn = int(burn.split('=')[1])
 
-    xmin = 0.25
-    xmax = 1
+    xmin = 0.00
+    xmax = 1.00
     L = datap.shape[0]
-    r = np.sqrt(datap['r**2'].values)[int(xmin*L): int(xmax*L)]
+    r   = np.sqrt(datap['r'].values)[int(xmin*L): int(xmax*L)]
     psi = datap['psi'].values[int(xmin*L): int(xmax*L)]
-    n0 = datap['N0'].values[int(xmin*L): int(xmax*L)]
-    n1 = datap['N1'].values[int(xmin*L): int(xmax*L)]
-    t = datap['time'].values[int(xmin*L): int(xmax*L)]
+    n0  = datap['pop0'].values[int(xmin*L): int(xmax*L)]
+    n1  = datap['pop1'].values[int(xmin*L): int(xmax*L)]
+    t   = datap['time_elapsed'].values[int(xmin*L): int(xmax*L)]
+    dt  = datap['dt'].values[int(xmin*L): int(xmax*L)]
 
     fig = plt.figure()
-    ax = fig.add_subplot(211, facecolor='yellow')
-    ax2 = fig.add_subplot(212)
+    ax = fig.add_subplot(311, facecolor='yellow')
+    ax2 = fig.add_subplot(312)
+    ax3 = fig.add_subplot(313)
     ax.set_title(title)
-    ax2.set_ylim(0, 1)
+    ylim = max(max(psi), max(r))
+    ax2.set_ylim(0, ylim)
+    ax3.set_ylim(0, ylim)
 
     ax.set_xlim(t[0], t[-1])
     ax.set_ylim(0, N)
@@ -46,8 +49,9 @@ for filename in sys.argv[1:]:
     ax2.set_xlabel('Time')
     ax2.set_ylabel('Order parameter r')
 
-    # ax.axvline(t[BURN], color='red', lw=2)
-    # ax2.axvline(t[BURN], color='red', lw=2)
+    if (burn < xmax*L) and (burn > xmin*L):
+        ax.axvline(t[burn - int(xmin*L)], color='red', lw=2)
+        ax2.axvline(t[burn - int(xmin*L)], color='red', lw=2)
 
     n0 = [(t[0], 0)] + list(zip(t, n0 + n1)) + [(t[-1], 0)]
     n1 = [(t[0], 0)] + list(zip(t, n1)) + [(t[-1], 0)]
@@ -57,7 +61,9 @@ for filename in sys.argv[1:]:
     ax.add_patch(poly1)
     ax.add_patch(poly2)
     ax2.plot(t, r, 'b-')
+    ax3.plot(t, psi, 'r-')
 
+plt.tight_layout()
 # plt.savefig('foooo.png')
 plt.show()
 
