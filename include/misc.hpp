5cd52260b76d5cef466bd65c364d21fc38310ed2
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2021-01-21 08:56:25 -0300

    move median function into misc header

--- include/misc.hpp
+++ include/misc.hpp
@@ -4,6 +4,20 @@
 #include <string>
 #include <algorithm>
 
+
+uint8_t median(size_t n0, size_t n1, size_t n2)
+{
+    // return the phase value of the majority, selecting randomly on ties.
+    if (n0 > n2 && n0 > n1)       return 0;
+    if (n1 > n2 && n1 > n0)       return 1;
+    if (n2 > n0 && n2 > n1)       return 2;
+    if ((n0 == n1) && (n1 == n2)) return rand() % 3;
+    if (n0 == n1)                 return rand() % 2;
+    if (n1 == n2)                 return rand() % 2 + 1;
+    if (n0 == n2)                 return (rand() % 2)? 0 : 2;
+    return 0;
+}
+
 bool cmdOptionExists(char** begin, char** end, const std::string& option)
 {
     return std::find(begin, end, option) != end;
